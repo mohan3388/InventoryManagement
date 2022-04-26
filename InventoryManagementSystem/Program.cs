eeb69a6d938a505ed0a0fbbe9d82254d5d4de530
mohan338 <sahumohan338@gmail.com>
2022-04-27 01:12:00 +0530

    [mohan]add: inventory management

---
+++ InventoryManagementSystem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace InventoryManagementSystem;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        const string INVENTORY_FILE_PATH = @"D:\dotnet\inventorymanage\Inventory\invent.json";
+        InventoryManagement invent = new InventoryManagement();
+        bool check = true;
+        Console.WriteLine("inventory program");
+        Console.WriteLine("press 1 to diplay");
+        while (true)
+        {
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    invent.DisplayInventory(INVENTORY_FILE_PATH);
+                    break;
+
+                case 0:
+                    check = false;
+                    break;
+            }
+        }
+    }
+}
